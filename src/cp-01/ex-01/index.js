c26ce552cb1bcb6cf4ba2fb51f1c4287af88073a
Conan Kunhwan Ahn <kunhwon@gmail.com>
2021-07-19 00:38:04 +0900

    초기 예제 코드 추가

---
+++ src/cp-01/ex-01/index.js
@@ -0,0 +1,8 @@
+const statement = require('./statement');
+
+const invoices = require('./invoices.json');
+const plays = require('./plays.json');
+
+(() => {
+  console.log(statement(invoices[0], plays));
+})();
