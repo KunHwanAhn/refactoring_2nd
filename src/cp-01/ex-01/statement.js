ca2ac1b8c0f75568a1f35ea632097d81f451808d
Conan Kunhwan Ahn <kunhwon@gmail.com>
2021-07-19 01:11:47 +0900

    반복문 쪼개기, 함수 추출하기 - totalVolumeCredits

--- src/cp-01/ex-01/statement.js
+++ src/cp-01/ex-01/statement.js
@@ -55,17 +55,24 @@ function statement(invoice, plays) {
     }).format(aNumber / 100);
   }
 
-  let totalAmount = 0;
-  let volumeCredits = 0;
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const perf of invoice.performances) {
+    // 포인트를 적립한다.
+      volumeCredits += volumeCreditsFor(perf);
+    }
+
+    return volumeCredits;
+  }
+
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  let totalAmount = 0;
 
   // eslint-disable-next-line no-restricted-syntax
   for (const perf of invoice.performances) {
     // const play = playFor(perf); // TODO: 여기서 변수 인라인 하는데... 적절한가?
     // const thisAmount = amountFor(perf);  // TODO: 여기서도 계산 하는 값을 재사용 안하고, 매번 계산하는 방향으로 변경함.
-
-    // 포인트를 적립한다.
-    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
@@ -73,7 +80,8 @@ function statement(invoice, plays) {
   }
 
   result += `총액 ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  // TODO: 반복문 쪼개기 & 함수 추출하기로 분리했는데, 반복문을 쪼갰지만 시간 복잡도는 여전히 O(N)이라서 성능적으로는 문제 없어보이나 어색..
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 
   return result;
 }
